97b711b12f655524172aabc1c6c6bac85767fa3b
rfhager <107468406+rfhager@users.noreply.github.com>
2023-12-10 16:14:51 +0100

    Add project files.

---
+++ GradeCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace GradeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Vill du beräkna betyg för en elev? Ange: JA/NEJ");
+                string userInput = Console.ReadLine().ToLower();
+                if (userInput == "ja")
+                {
+                    Console.WriteLine("Ange elevens poäng:");
+                    int points = int.Parse(Console.ReadLine());
+
+                    if (points >= 90)
+                        Console.WriteLine("Elevens betyg för denna uppgift är: A");
+                    else if (points >= 80)
+                        Console.WriteLine("Elevens betyg för denna uppgift är: B");
+                    else if (points >= 70)
+                        Console.WriteLine("Elevens betyg för denna uppgift är: C");
+                    else if (points >= 60)
+                        Console.WriteLine("Elevens betyg för denna uppgift är: D");
+                    else if (points >= 50)
+                        Console.WriteLine("Elevens betyg för denna uppgift är: E");
+                    else
+                        Console.WriteLine("Elevens betyg för denna uppgift är: F");
+
+                } 
+                else if (userInput == "nej")
+                    Environment.Exit(0);
+                else
+                    Console.WriteLine("Ogiltig input");
+
+            } while (true);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Variabler och Datatyper:
+
+            //int value = 42;
+            //Console.WriteLine(value);
+
+
+            //2. Strängar:
+
+            //string mysteryInformation = "This mystery has following amount of parts left: ";
+            //string mysteryDetails = "One bilion trilion gazilion.";
+            //Console.WriteLine(mysteryInformation + mysteryDetails);
+
+
+            //3. If-satser:
+
+            //Console.WriteLine("Enter the current temprature in celcius:");
+            //int temprature = Convert.ToInt32(Console.ReadLine());
+            //if (temprature == 0)
+            //{
+            //    Console.WriteLine("Well that is not a lot is it?!");
+            //}
+            //else if (temprature < 0)
+            //{
+            //    Console.WriteLine("Brrr, freaking cold!!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hope you have a great day!");
+            //}
+
+
+            //4. Loopar:
+
+            //for (int i = 2; i <= 20; i += 2)
+            //{
+            //    Console.WriteLine(i);
+            //} 
+            //for (int i = 1; i <= 20; i ++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //5. Arrayer:
+
+            //int[] ints = { 5, 5, 5, 5, 5 };
+
+            //for (int i = 0; i < ints.Length; i++)
+            //{
+            //    Console.WriteLine(ints[i]);
+            //}
+
+
+            //6. Variabler och Datatyper:
+
+            //int number = 5;
+            //double decimalNumber = 0.24D;
+            //char letter = 'A';
+            //string text = "Important information:";
+
+            //Console.WriteLine(text + "\n" + number + " " + letter + " " + decimalNumber);
+
+            //7. Strängar:
+
+            //Console.WriteLine("Vad önskar du dig i present?");
+            //string userWish = Console.ReadLine();
+            //string reverse = String.Empty;
+
+            //for (int i = userWish.Length-1; i >= 0; i--)
+            //{
+            //    reverse += userWish[i];
+            //}
+
+            //Console.WriteLine(reverse);
+
+
+            //8. If - satser:
+
+            //Console.WriteLine("Du kommer nu att få skriva in två tal.");
+            //Console.WriteLine("Skriv in ditt första tal:");
+            //int firstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Skriv in ditt andra tal:");
+            //int secondNumber = int.Parse(Console.ReadLine());
+
+            //if (firstNumber == secondNumber)
+            //{
+            //    Console.WriteLine("Du har valt två tal med samma värde.");
+            //}
+            //else if (firstNumber > secondNumber) 
+            //{
+            //    Console.WriteLine("Det första talet du valde har ett högre värde än det andra.");
+            //}
+            //else 
+            //{
+            //    Console.WriteLine("Det andra talet du valde har ett lägre värde än det första.");
+            //}
+
+
+            //9. Loopar:
+
+            //Console.WriteLine("Ange ett heltal mellan 1 och 10:");
+            //int userInt = int.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //Console.WriteLine("Multiplikationstabellen (1-10) för ditt valda tal:");
+
+            //for (int i = 1; i <=10; i++)
+            //{
+            //    Console.WriteLine(userInt*i);
+            //}
+
+            //int multiplication = 1;
+
+            //while (multiplication <= 10)
+            //{
+            //    Console.WriteLine(userInt*multiplication);
+            //    multiplication++;
+            //}
+
+
+            //10. Arrayer:
+
+            //string[] fruits = { "Äpple", "Päron", "Banan", "Jordgubbe", "Apelsin", "Mango" };
+
+            //foreach (string fruit in fruits)
+            //{
+            //    Console.WriteLine(fruit);
+            //}
+
+
+            //11. Random:
+
+            //Random rand = new Random();
+            //int number = rand.Next(1, 101);
+            //double decimalNumber = rand.NextDouble();
+
+            //Console.WriteLine(number);
+            //Console.WriteLine();
+            //Console.WriteLine(decimalNumber);
+        }
+    }
+}
